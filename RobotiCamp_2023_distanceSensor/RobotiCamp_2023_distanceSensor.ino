613f2dc739b70dc19df88aa60651000617fbb31b
Frédéric Druppel <7109145+fred-corp@users.noreply.github.com>
2023-08-03 09:52:40 +0200

    Create RobotiCamp_2023_distanceSensor.ino

---
+++ RobotiCamp_2023_distanceSensor/RobotiCamp_2023_distanceSensor.ino
@@ -0,0 +1,34 @@
+#define TRIG 7
+#define ECHO 8
+
+
+void setup() {
+  pinMode(TRIG, OUTPUT);
+  pinMode(ECHO, INPUT);
+
+  Serial.begin(9600);
+}
+
+void loop() {
+  float distance = distanceSensor();
+  
+  // Show distance on console
+  Serial.println(distance);
+}
+
+float capteurDistance() {
+  // Trigger sentor
+  digitalWrite(TRIG, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(TRIG, LOW);
+  delayMicroseconds(10);
+
+  // Measure ECHO duration
+  int time = pulseIn(ECHO, HIGH);
+
+  // Calculate distance
+  float distance = ((time * 340.0 / 10000) / 2.0);
+
+  // Return distance value
+  return distance;
+}
